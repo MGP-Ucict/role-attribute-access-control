5f755552dac9c1a35150ba1b8a204d0cbe0849e2
MGP-Ucict <penelova.maria@gmail.com>
2021-05-24 08:44:12 +0300

    Fix

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
-Route::group(['prefix' => 'admin', 'middleware' => ['web','can.access']], function () {
-	Route::resource('permissions', '\LaravelHrabac\AccessControl\Controllers\PermissionController')->except('show');
-	Route::resource('roles', '\LaravelHrabac\AccessControl\Controllers\RoleController')->except('show');
-	Route::resource('users', '\LaravelHrabac\AccessControl\Controllers\UserController')->except('show');
+Route::group(['prefix' => 'admin', 'middleware' => ['web']], function () {
+	Route::resource('permissions', '\LaravelHrabac\AccessControl\Controllers\PermissionController')->except('show')->middleware('can.access');
+	Route::resource('roles', '\LaravelHrabac\AccessControl\Controllers\RoleController')->except('show')->middleware('can.access');
+	Route::resource('users', '\LaravelHrabac\AccessControl\Controllers\UserController')->except('show')->middleware('can.access');
 });
 
